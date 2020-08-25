c522c15118a98089c9db074b35c70cdc79a50e1f
Duncan Gates <gatesdu@oregonstate.edu>
2020-08-24 18:20:21 -0700

    creating files

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -1,8 +1,8 @@
 library(datadrivencv)
 library(here)
 use_datadriven_cv(
-  full_name = "Nick Strayer",
-  data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
+  full_name = "Duncan Gates",
+  data_location = "https://docs.google.com/spreadsheets/d/1-Q9sgX068b-hk8uvw15_sjtrzla7l_2pGGbW2HpJH4U/edit#gid=917338460",
   pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
   html_location = "nickstrayer.me/cv/",
   source_location = "https://github.com/nstrayer/cv",
